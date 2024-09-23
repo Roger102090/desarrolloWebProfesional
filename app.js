6b45ead845b21bf5f5a42b68b09c088335bea0e1
Rogelio <roger.g.m@hotmail.com>
2024-09-22 21:59:08 -0600

    test

---
+++ app.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const menu = document.querySelector(".menu");
+    const dropdown = document.querySelector(".dropdown");
+
+    // Agregar evento para mostrar y ocultar el menú
+    menu.addEventListener("mouseover", function() {
+        dropdown.classList.add("show");
+    });
+    dropdown.addEventListener("mouseover", function() {
+        dropdown.classList.add("show");
+    });
+
+    menu.addEventListener("mouseout", function() {
+        dropdown.classList.remove("show");
+    });
+    dropdown.addEventListener("mouseout", function() {
+        dropdown.classList.remove("show");
+    });
+});
